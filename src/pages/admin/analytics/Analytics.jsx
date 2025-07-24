50e5b2689baa2a35da2bb78327875e73aca62b70
AnilHasda <hasdaanil098@gmail.com>
2025-07-24 19:38:57 +0545

    add admin remaining pages

---
+++ src/pages/admin/analytics/Analytics.jsx
@@ -0,0 +1,13 @@
+
+const Analytics = () => {
+  return (
+    <div
+      className=" w-full px-[72px] pt-[65px]"
+      style={{
+        background: "linear-gradient(to bottom, #000000, #202020)",
+      }}
+    ></div>
+  );
+};
+
+export default Analytics;
---
+++ src/pages/admin/live-management/LiveManagement.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const LiveManagement = () => {
+  return (
+    <div
+      className=" w-full px-[72px] pt-[65px]"
+      style={{
+        background: "linear-gradient(to bottom, #000000, #202020)",
+      }}
+    ></div>
+  );
+};
+
+export default LiveManagement;
